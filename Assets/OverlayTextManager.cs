4a4f448b7bb246737127aec8b4a364efd231c0af
Jake Martin <Jake3.Martin@live.uwe.ac.uk>
2024-03-17 17:33:50 +0000

    perk description boxes

---
+++ Assets/OverlayTextManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OverlayTextManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string titleMessage;
+    public string descriptionMessage;
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        Overlaybox.overlayBox.SetToolTipTitle(titleMessage, descriptionMessage);
+    
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        Overlaybox.overlayBox.HideToolTip();
+    }
+}
---
+++ Assets/Overlaybox.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Overlaybox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI descriptionText;
+
+
+    public static Overlaybox overlayBox;
+
+
+    private void Awake()
+    {
+        if(overlayBox != null && overlayBox != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            overlayBox = this;
+        }
+    }
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void SetToolTipTitle(string title, string description)
+    {
+        gameObject.SetActive(true);
+        titleText.text = title;
+        descriptionText.text = description;
+
+    }
+
+
+    public void HideToolTip()
+    {
+        gameObject.SetActive(false);
+        titleText.text = string.Empty;
+        descriptionText.text = string.Empty;
+
+    }
+}
