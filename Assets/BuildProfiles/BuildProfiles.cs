cd7ec02a58c0921c479572bf9b1e14048c21a6cf
Jake Martin <Jake3.Martin@live.uwe.ac.uk>
2024-03-19 11:55:33 +0000

    Update to overlay box to work with build profiles

--- Assets/BuildProfiles/BuildProfiles.cs
+++ Assets/BuildProfiles/BuildProfiles.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 public class BuildProfile : ScriptableObject
 {
     public string profileName;
+    public string profileDescription;
     public SkillData[] recommendedSkills; // List of skills recommended for this build
 }
--- Assets/OverlayTextManager.cs
+++ Assets/OverlayTextManager.cs
@@ -1,25 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+
+public enum DataType
+{
+    Skill,
+    BuildProfile
+}
 
 public class OverlayTextManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private string titleMessage;
     private string descriptionMessage;
 
+    
+
     public SkillData SkillData;
+    public BuildProfile BuildProfile;
+
+    public DataType CurrentDataType;
 
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        titleMessage = SkillData.skillName;
-        descriptionMessage = SkillData.description;
-        Overlaybox.overlayBox.SetToolTipTitle(titleMessage, descriptionMessage);
-    
+        switch (CurrentDataType)
+        {
+            case DataType.Skill:
+                ShowSkillTooltip();
+                break;
+            case DataType.BuildProfile:
+                ShowBuildProfileTooltip();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ShowSkillTooltip()
+    {
+        if (SkillData != null)
+        {
+            string titleMessage = SkillData.skillName;
+            string descriptionMessage = SkillData.description;
+            Overlaybox.overlayBox.SetToolTipTitle(titleMessage, descriptionMessage);
+        }
+    }
+
+    private void ShowBuildProfileTooltip()
+    {
+        if (BuildProfile != null)
+        {
+            string titleMessage = BuildProfile.profileName;
+            string descriptionMessage = BuildProfile.profileDescription;
+            Overlaybox.overlayBox.SetToolTipTitle(titleMessage, descriptionMessage);
+        }
     }
 
     public void OnPointerExit(PointerEventData pointerEventData)
     {
         Overlaybox.overlayBox.HideToolTip();
-    }
+    }
 }
+
+
+
+
+    /*
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        titleMessage = SkillData.skillName;
+        descriptionMessage = SkillData.description;
+        Overlaybox.overlayBox.SetToolTipTitle(titleMessage, descriptionMessage);
+    }
+
+
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        Overlaybox.overlayBox.HideToolTip();
+    }    */
